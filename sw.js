d8f9ba0b5aac2c5751fa9ad7574b3a7193621d84
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-05 22:08:01 -0300

    Usar imagens clássicas locais sem ZIP ou TAR

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
-const CACHE="hildegarda-v10";
-const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg"];
+const CACHE="hildegarda-v11";
+const A=[
+"./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg",
+"./assets/img/hero.jpg",
+"./assets/img/day1.jpg","./assets/img/day2.jpg","./assets/img/day3.jpg",
+"./assets/img/day4.jpg","./assets/img/day5.jpg","./assets/img/day6.jpg",
+"./assets/img/day7.jpg","./assets/img/day8.jpg","./assets/img/day9.jpg"
+];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  const url=new URL(e.request.url);
-  if(url.origin!==location.origin) return;
-  e.respondWith(fetch(e.request).then(res=>{
-    const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return res;
-  }).catch(()=>caches.match(e.request)));
+ if(e.request.method!=="GET") return;
+ const url=new URL(e.request.url);
+ if(url.origin!==location.origin) return;
+ e.respondWith(fetch(e.request).then(res=>{
+   const cp=res.clone(); caches.open(CACHE).then(c=>c.put(e.request,cp)); return res;
+ }).catch(()=>caches.match(e.request)));
 });
