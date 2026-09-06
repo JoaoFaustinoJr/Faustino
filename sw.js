e70a3ae9a0f73f18b4fea230f85459c8642d5ce5
JoaoFaustinoJr <108546564+JoaoFaustinoJr@users.noreply.github.com>
2026-09-05 21:08:30 -0300

    Usar imagens clássicas individuais e remover dependência do sprite

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-const CACHE="hildegarda-v8";const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg","./assets/classic-sprite-hq.webp"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;if(new URL(e.request.url).origin!==location.origin)return;e.respondWith(fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match(e.request)))})
+const CACHE="hildegarda-v9";
+const A=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon.svg",
+"./assets/img/hero.webp","./assets/img/day1.webp","./assets/img/day2.webp","./assets/img/day3.webp",
+"./assets/img/day4.webp","./assets/img/day5.webp","./assets/img/day6.webp","./assets/img/day7.webp",
+"./assets/img/day8.webp","./assets/img/day9.webp"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;if(new URL(e.request.url).origin!==location.origin)return;
+e.respondWith(fetch(e.request).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return res}).catch(()=>caches.match(e.request)))})
